bfd5318d83406ea7b380cf6f2a1e9b86bd6e95e8
Shaurya Seth <shauryaseth@hotmail.com>
2020-01-12 17:44:19 +0530

    Update ch03.lisp

--- ch03.lisp
+++ ch03.lisp
@@ -1,3 +1,94 @@
 ;; Chapter 3: Lists
 
+(defun our-listp (x)
+  (or (null x) (consp x)))
 
+(defun our-atom (x)
+  (not (consp x)))
+
+(defun our-equal (x y)
+  (or (eql x y)
+      (and (consp x)
+           (consp y)
+           (our-equal (car x) (car y))
+           (our-equal (cdr x) (cdr y)))))
+
+(defun our-copy-list (lst)
+  (if (atom lst)
+      lst
+      (cons (car lst) (our-copy-list (cdr lst)))))
+
+(defun compress (x)
+  (if (consp x)
+      (compr (car x) 1 (cdr x))
+      x))
+
+(defun compr (elt n lst)
+  (if (null lst)
+      (list (n-elts elt n))
+      (let ((next (car lst)))
+        (if (eql next elt)
+            (compr elt (+ n 1) (cdr lst))
+            (cons (n-elts elt n)
+                  (compr next 1 (cdr lst)))))))
+
+(defun n-elts (elt n)
+  (if (> n 1)
+      (list n elt)
+      elt))
+
+(defun uncompress (lst)
+  (if (null lst)
+      nil
+      (let ((elt (car lst))
+            (rest (uncompress (cdr lst))))
+        (if (consp elt)
+            (append (apply #'list-of elt)
+                    rest)
+            (cons elt rest)))))
+
+(defun list-of (n elt)
+  (if (zerop n)
+      nil
+      (cons elt (list-of (- n 1) elt))))
+
+(defun our-nthcdr (n lst)
+  (if (zerop n)
+      lst
+      (our-nthcdr (- n 1) (cdr lst))))
+
+(defun our-copy-tree (tr)
+  (if (atom tr)
+      tr
+      (cons (our-copy-tree (car tr))
+            (our-copy-tree (cdr tr)))))
+
+(defun our-subst (new old tree)
+  (if (eql tree old)
+      new
+      (if (atom tree)
+          tree
+          (cons (our-subst new old (car tree))
+                (our-subst new old (cdr tree))))))
+
+(defun len (lst)
+  (if (null lst)
+      0
+      (+ (len (cdr lst)) 1)))
+
+(defun our-member-if (fn lst)
+  (and (consp lst)
+       (if (funcall fn (car lst))
+           lst
+           (our-member-if fn (cdr lst)))))
+
+(defun mirror? (s)
+  (let ((len (length s)))
+    (and (evenp len)
+         (let ((mid (/ len 2)))
+           (equal (subseq s 0 mid)
+                  (reverse (subseq s mid)))))))
+
+(defun nthmost (n lst)
+  (nth (- n 1)
+       (sort (copy-list lst) #'>)))
